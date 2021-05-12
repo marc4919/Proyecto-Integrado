d6e3956dd39538bff8a9e61884b31dbab17e205f
Juan Carlos <76178594+JuanCarlos023@users.noreply.github.com>
2021-05-12 09:49:38 +0200

    Delete Codigosº directory

--- Codigosº/Miproyecto/clases/cerrarSesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start(); //Siempre que queramos usar un destroy hay que poner un start 
-session_destroy(); //Aqui lo que hacemos es al destruir la sesion la cerramos y nos dirige a login
-header("Location: ../proyecto.php");
-
