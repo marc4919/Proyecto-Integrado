23fbdfc544015904eb10d7345f6bb54f004d9317
Marc4919 <marcgeek4919@gmail.com>
2021-05-14 09:24:10 +0200

    Refresco Planes Principales

--- Planme/src/galleryPlan.php
+++ Planme/src/galleryPlan.php
@@ -3,7 +3,7 @@
 class galleryPlan extends Plan
 {
 
-    // AÑADIR AJAX PARA EVITAR RECARGA DE PÁGINA
+    // AÑADIR AJAX PARA EVITAR RECARGA DE PÁGINA - MGT
     public function getPlans()
     {
         $number = rand(1, 24);
@@ -19,28 +19,88 @@ class galleryPlan extends Plan
 
     public function showPlans()
     {
+        // AÑADIR UN REINICIO DE ARRAY CUANDO ESTÁN TODOS GUARDADOS --
+        $planesAparecidos = []; // LA MAGIA - MGT
         for ($i = 0; $i <= 2; $i++) {
             $result = $this->getPlans();
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    echo "<div>";
-                    echo "<p>" . $row["Nombre"] . "</p>";
-                    //echo "<p>" . $row["Descripcion"] . "</p>";
-                    echo "<p>" . $row["Localizacion"] . "</p>";
-                    echo "<p>" . $row["Transporte"] . "</p>";
-                    //echo "<p>" . $row["Categoria_Principal"] . "</p>";
-                    //echo "<p>" . $row["Categoria_Secundaria"] . "</p>";
-                    echo "<p>" . $this->formatoMoneda($row["Precio"]) . "</p>";
-                    echo "</div>";
+                    if (in_array($row["Nombre"], $planesAparecidos)) {
+                        $i--;
+                    } else {
+
+                        echo "<div>";
+                        echo "<p>" . $row["Nombre"] . "</p>";
+                        //echo "<p>" . $row["Descripcion"] . "</p>";
+                        echo "<p>" . $row["Localizacion"] . "</p>";
+                        echo "<p>" . $row["Transporte"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Principal"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Secundaria"] . "</p>";
+                        echo "<p>" . $this->formatoMoneda($row["Precio"]) . "</p>";
+                        echo "</div>";
+                        array_push($planesAparecidos, $row["Nombre"]);
+                    }
                 }
             } else {
                 echo "no hay resultados";
             }
         }
+        echo "</br>";
+        echo "<span id='Borrado'>Refresh</span>";
+        // HABRÍA QUE HACER OTRO MÉTODO PARA ESTO RETURNEANDO EL ARRAY $planesAparecidos --- MGT
+        /* for ($i = 0; $i <= 5; $i++) {
+            $result = $this->getPlans();
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    if (in_array($row["Nombre"], $planesAparecidos)) {
+                        $i--;
+                    } else {
+
+                        echo "<div>";
+                        echo "<p>" . $row["Nombre"] . "</p>";
+                        //echo "<p>" . $row["Descripcion"] . "</p>";
+                        echo "<p>" . $row["Localizacion"] . "</p>";
+                        echo "<p>" . $row["Transporte"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Principal"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Secundaria"] . "</p>";
+                        echo "<p>" . $this->formatoMoneda($row["Precio"]) . "</p>";
+                        echo "</div>";
+                        array_push($planesAparecidos, $row["Nombre"]);
+                    }
+                }
+            } else {
+                echo "no hay resultados";
+            }
+        } */
     }
 
     public function updatePlans()
     {
+        $planesAparecidos = []; // LA MAGIA - MGT
+        for ($i = 0; $i <= 2; $i++) {
+            $result = $this->getPlans();
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    if (in_array($row["Nombre"], $planesAparecidos)) {
+                        $i--;
+                    } else {
+
+                        echo "<div>";
+                        echo "<p>" . $row["Nombre"] . "</p>";
+                        //echo "<p>" . $row["Descripcion"] . "</p>";
+                        echo "<p>" . $row["Localizacion"] . "</p>";
+                        echo "<p>" . $row["Transporte"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Principal"] . "</p>";
+                        //echo "<p>" . $row["Categoria_Secundaria"] . "</p>";
+                        echo "<p>" . $this->formatoMoneda($row["Precio"]) . "</p>";
+                        echo "</div>";
+                        array_push($planesAparecidos, $row["Nombre"]);
+                    }
+                }
+            } else {
+                echo "no hay resultados";
+            }
+        }
     }
 
     public function savePlans()
