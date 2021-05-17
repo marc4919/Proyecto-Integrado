8a51e0ba9e4a1e61fcb75b0c51eec3e485badd7d
DESKTOP-B1QPCEG\Juan Carlos <jurolo@floridauniversitaria.es>
2021-05-17 22:50:03 +0200

    AÑADIDO | Page.js

---
+++ Planme/public/js/page.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+   
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Valencia", "Turismo", "Diversión", "Plan-Me"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Valencia", "Turismo", "Diversión", "Plan-Me"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+  
+});
