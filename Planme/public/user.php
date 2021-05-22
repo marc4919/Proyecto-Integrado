732824e2722b8dcc3cd32642cb99d84ab65c3291
Pepitus <pepitusmaxi@gmail.com>
2021-05-22 16:44:14 +0200

    php

--- Planme/public/user.php
+++ Planme/public/user.php
@@ -24,7 +24,7 @@ $obje = new UserAdmin();
       <li><a href="inicio-user.php">Inicio</a></li>
       <li><a href="gallery-plans.php">Galeria</a></li>
       <li><img class="logo" src="img/inicio/logo.png"></li>
-      <li><a href="crear.html">Creador</a></li>
+      <li><a href="crear.php">Creador</a></li>
       <li>
         <div class="dropdown">
           <ul>
